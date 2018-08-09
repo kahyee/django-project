656baf86bd59a9c85ec82203c95ec2b94b1ea78f
Kahyee Fong <kahyeefong@gmail.com>
2018-08-08 21:01:38 -0700

    lesson 7 pre exclusion complete

--- django/mysite/myblog/admin.py
+++ django/mysite/myblog/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from myblog.models import Post
+from myblog.models import Post, Category
 
-admin.site.register(Post)
-
-# a new import
-from myblog.models import Category
-
-# and a new admin registration
+# admin.site.register(Post)
 admin.site.register(Category)
+
+class CategoriesInline(admin.TabularInline):
+    model = Category.posts.through
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoriesInline]
